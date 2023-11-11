7dc35e65a5fe388e92aa171809f55d36b2ae4c5e
mysycry <josiasmichael@gmail.com>
2023-11-12 05:40:03 +0800

    Initial commit

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'ca-central-1_tfhCZoZEg', // e.g. ca-central-1_tfhCZoZEg
+        userPoolClientId: '5p1qvn7oa5aernc0hphkfkcclj', // e.g. 5p1qvn7oa5aernc0hphkfkcclj
+        region: 'ca-central-1' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: 'https://wk0kun502m.execute-api.ca-central-1.amazonaws.com/dev' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
